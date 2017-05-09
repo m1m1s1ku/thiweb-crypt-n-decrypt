13ab03b9abdd684e34fb847b868dcbd276c29aa8
Ghostfly <ghostfly@outlook.com>
2017-05-10 01:53:45 +0200

    update chrome for chrome store

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,22 +4,14 @@
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-      //console.time("Exec");
       /*
-       *	Init
+       *   Inject.js
+       *   ThiWeb Chrome \ Edge \ Firefox
+       *   Automatically decrypt links on the forum
        */
       if (checkConnect()) {
         getAllLinks();
-      } else {
-        createNotification(
-          "Extension désactivée",
-          "https://forum.thiweb.com/styles/prosilver/theme/images/site_logo.png",
-          "Tu n'es pas connecté, pour limiter les appels API l'extension ne fonctionnera pas",
-          "https://forum.thiweb.com/ucp.php?mode=login"
-        );
       }
-      //console.timeEnd("Exec");
 
       /**
        * Check if the user is connected or not
@@ -28,14 +20,51 @@ chrome.extension.sendMessage({}, function(response) {
         if (document.getElementById('connectExt')) {
           return false;
         }
-        return true;
+        return document.getElementById('usernameExt').lastChild.innerHTML;
       }
 
       /**
-       * Create a Chrome notification
-       * @param title, icon, content, link : strings
+       * Get every code on the page
        */
+      function getAllLinks() {
+        var codes = document.getElementsByTagName('code');
+        for (var i = 0; i < codes.length; i++) {
+          codes[i].innerHTML = codes[i].innerHTML.trim();
+          if (codes[i].innerHTML.startsWith("TWL2")) {
+            getFromZerawApi(codes[i]);
+          }
+        }
+      }
+
+      /**
+       * Get user msg count
+       */
+      function getCount(username) {
+        var res;
+        const req = new XMLHttpRequest();
+
+        req.onreadystatechange = function(event) {
+          if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+              res = JSON.parse(this.responseText);
+              if (res.message >= 1) {
+                getAllLinks();
+              } else {
+                createNotification(
+                  "Salut " + username, null, "Pour toute demande d'aide, ou faire fonctionner l'extension, file te présenter ;)", "https://forum.thiweb.com/viewforum.php?f=2");
+              }
+            }
+          };
+        }
+
+        req.open("GET", "https://live.thiweb.com/api.php?posts&str=" + username, true);
+        req.send(null);
+      }
+
       function createNotification(title, icon, content, link) {
+        if (icon === null) {
+          icon = "https://forum.thiweb.com/styles/prosilver/theme/images/site_logo.png";
+        }
         let notification = new Notification(title, {
           icon: icon,
           body: content,
@@ -47,27 +76,14 @@ chrome.extension.sendMessage({}, function(response) {
       }
 
       /**
-       * Get every code on the page
-       */
-      function getAllLinks() {
-        let codes = document.getElementsByTagName('code');
-        for (var i = 0; i < codes.length; i++) {
-          codes[i].innerHTML = codes[i].innerHTML.trim();
-          if (codes[i].innerHTML.startsWith("TWL2")) {
-            getFromZerawApi(codes[i]);
-          }
-        }
-      }
-
-      /**
        * Parse every url and replace it with <a>
        * @param strParse : String to parse
        */
       function parseAndReplaceUrls(strParse) {
-        const re = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)/mgi;
+        var re = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)/mgi;
 
         if (strParse !== undefined && strParse.match(re)) {
-          let m;
+          var m;
           while ((m = re.exec(strParse)) !== null) {
             if (m.index === re.lastIndex) {
               re.lastIndex++;
@@ -86,33 +102,26 @@ chrome.extension.sendMessage({}, function(response) {
        * @param elem : HTML element
        */
       function getFromZerawApi(elem) {
-        let result;
-        let xmlhttp = new XMLHttpRequest();
+        const req = new XMLHttpRequest();
 
-        xmlhttp.onreadystatechange = function() {
-          if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-            if (xmlhttp.status == 200) {
-              var res = JSON.parse(xmlhttp.responseText);
+        req.onreadystatechange = function(event) {
+          if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+              var res = JSON.parse(this.responseText);
               res.message.raw = parseAndReplaceUrls(res.message.raw);
               if (res.message.raw !== undefined) {
                 elem.innerHTML = res.message.raw;
               } else {
                 elem.innerHTML = "Erreur pendant le décryptage automatique";
               }
-
             } else {
-              createNotification(
-                "Erreur interne",
-                "https://forum.thiweb.com/styles/prosilver/theme/images/site_logo.png",
-                "La réponse du serveur est incorrecte",
-                "mailto:ghostfly@outlook.com"
-              );
+              console.error("Erreur serveur", this.status, this.statusText);
             }
           }
-        }
+        };
 
-        xmlhttp.open("GET", "https://live.thiweb.com/api.php?decode&str=" + elem.innerHTML, true);
-        xmlhttp.send();
+        req.open("GET", "https://live.thiweb.com/api.php?decode&str=" + elem.innerHTML, true);
+        req.send(null);
       }
 
     };
