2bf049d4cf38faa7a173b286d9c11cf608d967a0
Léonard <leonard@bigsool.com>
2020-03-22 17:38:25 +0100

    code cleanup

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -165,18 +165,26 @@ class TWExtension {
      * @returns {string}
      */
     _activateLinks(str) {
+        if(!str){
+            return str;
+        }
+
         const re = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)/mgi;
-    
-        if (str !== undefined && str.match(re)) {
-            let m;
-            while ((m = re.exec(str)) !== null) {
-                if (m.index === re.lastIndex) {
-                    re.lastIndex++;
-                }
-    
-                for(const match of m){
-                    str = str.replace(match, "<a href=\"" + match + "\">" + match + "</a>");
-                }
+        if(!str.match(re)){
+            return str;
+        }
+
+        /**
+         * @type {RegExpExecArray}
+         */
+        let m;
+        while ((m = re.exec(str)) !== null) {
+            if (m.index === re.lastIndex) {
+                re.lastIndex++;
+            }
+
+            for(const match of m){
+                str = str.replace(match, "<a href=\"" + match + "\">" + match + "</a>");
             }
         }
     
@@ -224,6 +232,10 @@ class TWExtension {
 }
 
 (async function(){
-    const utils = new TWExtension();
-    await utils.run();
+    try {
+        const ext = new TWExtension();
+        await ext.run();
+    } catch (err){
+        console.error('Error while running ext', err);
+    }
 })();
