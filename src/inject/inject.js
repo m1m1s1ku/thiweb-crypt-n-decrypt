262080050d115f2edb3377f73d6880c955d37ac2
Léonard <leonard@bigsool.com>
2020-03-23 22:58:48 +0100

    avoid innerHTML usage (avoid firefox addons warn :D)

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -133,8 +133,9 @@ class TWExtension {
             let idx = 0;
             for(const code of this._codes){
                 if (this._clean(code.innerHTML) == coded[idx]) {
-                    code.innerHTML = this._activateLinks(decoded[idx]);
-                    code.animate([{ opacity: 0 },{ opacity: 1 }], { duration: 300 });
+                    const newCode = this._activateLinks(decoded[idx]);
+                    code.parentElement.replaceChild(newCode, code);
+                    newCode.animate([{ opacity: 0 },{ opacity: 1 }], { duration: 300 });
                     idx++;
                 }
             }
@@ -181,18 +182,12 @@ class TWExtension {
 
     /**
      * @param {string} str
-     * @returns {string}
+     * @returns {HTMLElement}
      */
     _activateLinks(str) {
-        if(!str){
-            return str;
-        }
+        const codeElement = document.createElement('code');
 
         const re = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)/mgi;
-        if(!str.match(re)){
-            return str;
-        }
-
         /**
          * @type {RegExpExecArray}
          */
@@ -206,8 +201,10 @@ class TWExtension {
                 str = str.replace(match, "<a href=\"" + match + "\">" + match + "</a>");
             }
         }
-    
-        return str;
+
+        codeElement.insertAdjacentHTML('beforeend', str);
+        
+        return codeElement;
     }
 
     /**
