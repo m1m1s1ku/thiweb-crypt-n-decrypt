f6684f70c0bf8f1ae2647e4507574e0b2170d7d4
Leonard <leonard@mimisiku.dev>
2023-02-17 23:26:26 +0100

    fix infinite replace of links

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -153,14 +153,20 @@ export default class TWExtension {
 
         const re = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)/mgi;
         let m: RegExpExecArray | null;
+        const links = new Set<string>();
+
         while ((m = re.exec(str)) !== null) {
             if (m.index === re.lastIndex) {
                 re.lastIndex++;
             }
 
             for(const match of m){
-                str = str.replace(match, "<a href=\"" + match + "\">" + match + "</a>");
+                links.add(match);
             }
+        }
+
+        for(const link of links) {
+            str = str.replace(link, "<a href=\"" + link + "\">" + link + "</a>");
         }
 
         this._insertUnsafeHTML(str, codeElement);
@@ -168,11 +174,7 @@ export default class TWExtension {
         return codeElement;
     }
     
-    _insertUnsafeHTML(str: string, element: HTMLElement, clear = false): HTMLElement {
-        if(clear){
-            element.innerHTML = '';
-        }
-
+    _insertUnsafeHTML(str: string, element: HTMLElement): HTMLElement {
         const parser = new DOMParser();
         const parsedBody = parser.parseFromString(str, 'text/html').body;
 
