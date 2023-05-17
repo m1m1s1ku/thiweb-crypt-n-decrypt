3d77f6e7bd34c528dddeef36a8e035b495f3f41e
Leonard <leonard@mimisiku.dev>
2023-05-17 23:05:47 +0200

    upgrade deps, prepare to fix firefox

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [
+    crx({ manifest })
+  ],
 })
